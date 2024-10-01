86c01b5c3f7aebc0d20212baba4e8938b20b6548
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-10-01 13:01:49 -0700

    more progress on UI stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,16 +11,17 @@ public class GameManager : MonoBehaviour
 
     public enum Stats
     {
-        RELOAD_TIME,DAMAG,MOVESMENT_SPEE, SPAWN_RATE
+        RELOAD_TIME,DAMAGE,MOVEMENT_SPEED, SPAWN_RATE
     }   
 
     //1:ReloadTime, 
     public static int[] levels;
     public static float[] stats;
-    public static float ReloadTime;
-    public static float  Damage;
-    public static float MovementSpeed;
-    public static float SpawnRate;
+    public static int TotalKills;
+    public static int KillsTillNextLevel = 2;
+    public static int CurrentKills = 0;
+
+    private float _timer = 120;
 
     void Awake() {
         if (instance == null) 
@@ -46,6 +48,8 @@ public class GameManager : MonoBehaviour
         {
             QuitGame();
         }
+        _timer -= Time.deltaTime;
+        float DisplayVal = Mathf.Round(_timer * 10) / 10;
     }
 
     public void RestartGame()
@@ -60,6 +64,50 @@ public class GameManager : MonoBehaviour
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #endif
+    }
+    public void PlusReload(){PlusStat(0);}
+    public void PlusDamage(){PlusStat(1);}
+    public void PlusSpeed(){PlusStat(2);}
+    public void PlusSpawnRate(){PlusStat(3);}
+
+    private void PlusStat(int index)
+    {
+        if (levels[index] != 4 && CurrentKills > KillsTillNextLevel)
+        {
+            levels[index]++;
+            stats[index] = 0;
+            //AudioManager.instance.Play([statupnoise]);
+            CurrentKills -= KillsTillNextLevel;
+            KillsTillNextLevel+=1;
+            if ( index == 0)
+            {
+                stats[index] *= 0.5f;
+            }
+            if (index == 1)
+            {
+                stats[index] +=1;
+            }
+            else
+            {
+                stats[index] *= 2;
+            }
+        }
+    }
+
+    public static void ZombieKilled()
+    {
+        TotalKills++;
+        CurrentKills++;
+    }
+
+    public static void GameOver()
+    {
+        
+    }
+
+    public static void TimeOut()
+    {
+
     }
 }
 
