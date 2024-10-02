aa067e31d874570580115529fded3251c94a15c4
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-10-01 20:24:53 -0700

    UI Stuff done I think

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public ScreenShake Camera;
+
     public enum Stats
     {
         RELOAD_TIME,DAMAGE,MOVEMENT_SPEED, SPAWN_RATE
@@ -20,19 +22,18 @@ public class GameManager : MonoBehaviour
     public static int[] levels;
     public static float[] stats;
     public static int TotalKills;
-    public static int KillsTillNextLevel = 2;
-    public static int CurrentKills = 0;
+    public static int KillsTillNextLevel;
+    public static int CurrentKills ;
 
     //All the UI Stuff
     [SerializeField] private TextMeshProUGUI _score;
     [SerializeField] private TextMeshProUGUI[] _stats;
     [SerializeField] private TextMeshProUGUI _timerText;
     [SerializeField] private TextMeshProUGUI _nextLevelText; 
-    private float _timer = 120;
+    private float _timer = 1f;
+    private bool _endTriggered = false;
 
     void Awake() {
-        CurrentKills = 100;
-        ZombieKilled();
         if (instance == null) 
         {
             instance = this;
@@ -41,9 +42,16 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
         levels = new int[]{1,1,1,1};
         stats = new float[]{0.5f,1f,1f,1f};
+    }
+
+    void Start()
+    {
+        TotalKills = 0;
+        CurrentKills = 0;
+        KillsTillNextLevel = 2;
     }
 
     // Update is called once per frame
@@ -58,6 +66,10 @@ public class GameManager : MonoBehaviour
             QuitGame();
         }
         _timer -= Time.deltaTime;
+        if (_timer <= 0 && !_endTriggered)
+        {
+            TimeOut();
+        }
         float DisplayVal = Mathf.Round(_timer * 10) / 10;
         _timerText.text = "Time: " + DisplayVal;
     }
@@ -65,6 +77,7 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        _endTriggered = false;
     }
 
     public void QuitGame()
@@ -110,14 +123,39 @@ public class GameManager : MonoBehaviour
         _score.text = TotalKills.ToString();
     }
 
-    public static void GameOver()
+    public void GameOver()
     {
-        
+        StartCoroutine(GameOverEnum());
     }
 
-    public static void TimeOut()
-    {
+    private IEnumerator GameOverEnum()
+    {   
+        Time.timeScale = 0f;
+        yield return new WaitForSeconds(2f);
+        RestartGame();
 
+    }
+    
+
+    public void TimeOut()
+    {
+        Time.timeScale = 0.0f;
+        _timerText.color = Color.red;
+        Camera.TriggerShake(2f);
+        _endTriggered = true;
+        _score.text = "Kills: " + TotalKills;
+        if (TotalKills < 20)
+        {
+            _score.text += " | 1/3 Stars";
+        }
+        else if (TotalKills < 40)
+        {
+            _score.text += " | 2/3 Stars";
+        }
+        else
+        {
+            _score.text += " | 3/3 Stars!!!!";
+        }
     }
 
     private String GetBar(int level)
